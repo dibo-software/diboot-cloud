2040fcb1231107c8396cee3408d444c48d534034
JerryMa1024 <mzc2003@qq.com>
2021-01-18 16:58:09 +0800

    优化超时时间

--- diboot-antd-admin/src/utils/request.js
+++ diboot-antd-admin/src/utils/request.js
@@ -25,7 +25,7 @@ let waitingQueue = []
 // 创建 axios 实例
 const service = axios.create({
   baseURL: BASE_URL, // api base_url
-  timeout: 6000 // 请求超时时间
+  timeout: 30000 // 请求超时时间
 })
 
 const err = (error) => {
