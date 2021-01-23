c12ff22536dd036861d3a6f038dea89d3a7d5b59
wuy <1311695042@qq.com>
2021-01-23 14:39:06 +0800

    optimize: 优化多余请求

--- diboot-antd-admin/src/views/system/iamUser/Index.vue
+++ diboot-antd-admin/src/views/system/iamUser/Index.vue
@@ -8,7 +8,7 @@
         <user-list :current-node-id="currentNodeId" ref="userList" />
       </a-col>
     </a-row>
-    <user-list :current-node-id="currentNodeId" ref="userList" />
+    <user-list v-permission-missing="['orgTree']" :current-node-id="currentNodeId" ref="userList" />
   </a-card>
 </template>
 
