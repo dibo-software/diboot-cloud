4e988b6bf4ecc74107b4f2171df6018cb983fe8b
wuy <1311695042@qq.com>
2021-05-14 15:52:47 +0800

    modify: 优化message初始化

--- diboot-modules/diboot-message/src/main/java/com/diboot/message/starter/MessageSqlInitializer.java
+++ diboot-modules/diboot-message/src/main/java/com/diboot/message/starter/MessageSqlInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020, www.dibo.ltd (service@dibo.ltd).
+ * Copyright (c) 2015-2021, www.dibo.ltd (service@dibo.ltd).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,19 +15,7 @@
  */
 package com.diboot.message.starter;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.diboot.cloud.config.Cons;
-import com.diboot.core.exception.BusinessException;
-import com.diboot.core.service.DictionaryService;
-import com.diboot.core.service.DictionaryServiceExtProvider;
-import com.diboot.core.util.ContextHelper;
-import com.diboot.core.util.JSON;
 import com.diboot.core.util.SqlFileInitializer;
-import com.diboot.core.util.V;
-import com.diboot.core.vo.DictionaryVO;
-import com.diboot.iam.entity.*;
-import com.diboot.iam.vo.IamResourcePermissionListVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -35,8 +23,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * 组件初始化
